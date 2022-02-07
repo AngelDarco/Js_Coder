1f87c8e8cd5d54b038a71fbd9553924e5584a92f
angeldarco <angel92-eme@hotmail.com>
2022-02-07 09:38:49 +0000

    Decoder add

--- app.js
+++ app.js
@@ -1,48 +1,53 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const texto = document.getElementById('codertxt');
-    const button = document.getElementById('coderbtn');
-    let array = [];
-    let contador = 0;
+    const btncode = document.getElementById('coderbtn');
+    const btndecode = document.getElementById('decoderbtn');
 
-    texto.addEventListener('keyup', (key) => { keysi(key) });
-    button.addEventListener('click', () => { coder(array) });
+    btncode.addEventListener('click', () => { coder(texto.value) });
+    btndecode.addEventListener('click', () => { decoder(texto.value) });
     const result = document.getElementById('result');
 
 
-    function keysi(k){
-        array[contador] = k.key;
-        contador++;
-    }
+    const abc = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+    function coder(code) {
 
-    function coder(codetxt) {
-        const abc = ['0','1','2','3','4','5','6','7','8','9',"A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+        let decode = code.toUpperCase().split('');
         reset();
 
-        for (let i = 0; i <= codetxt.length ; i++){
-            for(let x = 0; x <= abc.length-1 ; x++){
-                if(abc[x]==codetxt[i])
-                    if(abc[x+3]== undefined) result.value += abc[x-33]; 
-                        else result.value += abc[x+3];
+        for (let i = 0; i <= decode.length - 1; i++) {
+            if (decode[i] == ' ') result.innerHTML += '*';
+            for (let x = 0; x <= abc.length - 1; x++) {
+                if (abc[x] == decode[i]) {
+                    if (abc[x + 3] == undefined) result.innerHTML += abc[x - 34];
+                    else result.innerHTML += abc[x + 3];
                 }
+            }
 
         }
-        
 
-
-
-        
-
-        
     }
 
+    function decoder(code) {
+        reset();
+        let decode = code.toUpperCase().split('');
+        for (let i = 0; i <= decode.length - 1; i++) {
+            if (decode[i] == '*') result.innerHTML += ' ';
+            for (let x = 0; x <= abc.length - 1; x++) {
+                if (abc[x] == decode[i]) {
+                    if (abc[x - 3] == undefined) result.innerHTML += abc[x + 34];
+                    else result.innerHTML += abc[x - 3];
+                }
+            }
+        }
+    }
 
-    function reset(){
-        result.value = '';
+    function reset() {
+        result.innerHTML = '';
         texto.value = '';
         array = [];
-        contador = 0;        
+        contador = 0;
     }
 })
 
